bf672152566d764241e1e412b62e16ce63f06e94
liaoyjun <18916108830@163.com>
2018-04-28 21:17:17 +0800

    finished the adapter and the layout of the readActivity

--- app/src/main/java/layout/com/anew/easyItalian/read/ArticleAdapter.java
+++ app/src/main/java/layout/com/anew/easyItalian/read/ArticleAdapter.java
@@ -20,30 +20,28 @@ import layout.com.anew.easyItalian.R;
 
 public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHolder>{
 
-    //private  Context context;
+
     private List<Article> mList;
+
     public ArticleAdapter(Context context, List<Article> mList) {
-        //this.context = context;
         this.mList = mList;
     }
+
     static class ViewHolder extends RecyclerView.ViewHolder{
 
-        //private View articleView;
+        private View articleView;
         private TextView title;
-        //private TextView level;
-        //private TextView text;
+        private TextView level;
+        private TextView text;
         //private ImageView image;
-
-
         public ViewHolder(View itemView) {
+
             super(itemView);
-
             title = (TextView) itemView.findViewById(R.id.title);
-            //level = (TextView) itemView.findViewById(R.id.level);
-            //text = (TextView) itemView.findViewById(R.id.text);
-
+            level = (TextView) itemView.findViewById(R.id.level);
+            text = (TextView) itemView.findViewById(R.id.text);
             //image = (ImageView) itemView.findViewById(R.id.image);
-            //articleView=itemView;
+            articleView=itemView;
         }
     }
 
@@ -56,15 +54,13 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
     }
 
     public void onBindViewHolder(ViewHolder holder, int position) {
-        /**
-         * 给itemView设置tag
-         */
+
         Article article =mList.get(position);
         //holder.itemView.setTag(position);
         holder.title.setText(article.getTitle());
-        //holder.level.setText(article.getLevel());
-        //holder.text.setText(article.getText());
-        //    holder.image.setImageResource(article.getimageUrl());
+        holder.level.setText(article.getLevel());
+        holder.text.setText(article.getText());
+        // holder.image.setImageResource(article.getimageUrl());
     }
 
 
@@ -72,37 +68,40 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
 
 
         //View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.article_item,parent,false);
-
-        View view = View.inflate(parent.getContext(), android.R.layout.simple_list_item_1, null);
-
+        View view = View.inflate(parent.getContext(), R.layout.article_item2, null);
         final ViewHolder holder = new ViewHolder(view);
 
-        /*
+        //点击效果
         holder.articleView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int position=holder.getAdapterPosition();
                 Article article=mList.get(position);
-                Toast.makeText(v.getContext(),"you click item"+article.getTitle(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(v.getContext(),"you click item "+position,Toast.LENGTH_SHORT).show();
             }
         });
 
-        holder.image.setOnClickListener(new View.OnClickListener(){
+        //点击title
+        holder.title.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View v) {
                 int position=holder.getAdapterPosition();
                 Article article=mList.get(position);
-                Toast.makeText(v.getContext(),"you click image of"+article.getTitle(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(v.getContext(),"you click title of item "+position+article.getTitle(),Toast.LENGTH_SHORT).show();
             }
         });
-        */
+
+        //点击正文
+        holder.text.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+                int position=holder.getAdapterPosition();
+                Article article=mList.get(position);
+                Toast.makeText(v.getContext(),"you click text of item "+position+article.getTitle(),Toast.LENGTH_SHORT).show();
+            }
+        });
         return holder;
     }
-
-
-
-
-
 
 
 }
--- app/src/main/java/layout/com/anew/easyItalian/read/ReadActivity.java
+++ app/src/main/java/layout/com/anew/easyItalian/read/ReadActivity.java
@@ -5,6 +5,7 @@ import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,38 +22,49 @@ import java.util.List;
 import layout.com.anew.easyItalian.R;
 
 public class ReadActivity extends Activity {
-    private  List<String> listT=new ArrayList<>();
+
 
     private List<Article> articleList=new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(ReadActivity.this);
 
-      //  recyclerView.setAdapter();
 
+        //初始化数据
         initArticle();
-       // Toast.makeText(ReadActivity.this,"hh",Toast.LENGTH_LONG).show();
+
+        //将articleTist里的文章展示出来
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(layoutManager);
         ArticleAdapter adapter=new ArticleAdapter(ReadActivity.this,articleList);
-       // RecyclerView recyclerView=findViewById(R.id.recyclerView);
         recyclerView.setAdapter(adapter);
 
     }
 
+    //初始化数据
     private void initArticle(){
-
-
-
-
-        for(int
-            i=0;i<5;i++){
-            String title="title1";
-            String level="4";
-            String text="text1";
+        for(int i=0;i<5;i++){
+            String title="Amo la mia mamma e il mio papa  ";
+            String level="4星";
+            String text="Per la maggior parte del mondo cristiano, il culmine della" +
+                    " festività natalizia è il cenone di Natale. è il momento in cui le " +
+                    "famiglie si riuniscono per festeggiare la vigilia di Natale che" +
+                    " normalmente si celebra dopo la Messa di mezzanotte. In Francia " +
+                    "questo pasto viene chiamato \"le reveillon\". Il cenone natalizio " +
+                    "varia secondo le tradizioni culinarie, il menu consiste in anatra, " +
+                    "paste fatte con farina di grano saraceno con panna acida, tacchino con " +
+                    "castagne, mentre per i parigini, ostriche e Foie Gras. Una torta che " +
+                    "porta il nome di \"La Bouche de Noel\" con la forma di un tronco d'albero" +
+                    " viene consumata durante la reveillon. Lo Champagne, il vino frizzante " +
+                    "prodotto nelle regione denominata Champagne, viene bevuto per celebrare " +
+                    "entrambe le festività di Natale e di Capodanno."+i;
             String id="id"+i;
             Article article=new Article(id,title,level,text,"image1");
 
+            //将article放入链表中
             articleList.add(article);
         }
     }
