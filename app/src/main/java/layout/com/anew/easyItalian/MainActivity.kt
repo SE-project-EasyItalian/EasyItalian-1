46d7a17477ba85dec80ab44c352eb62958a786cb
xsxun <xsxuno@outlook.com>
2018-06-06 20:50:40 +0800

    separate look up word

--- app/src/main/java/layout/com/anew/easyItalian/MainActivity.kt
+++ app/src/main/java/layout/com/anew/easyItalian/MainActivity.kt
@@ -15,24 +15,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import android.view.View
 import android.widget.TextView
-import com.avos.avoscloud.AVObject
-import com.avos.avoscloud.AVQuery
 import com.avos.avoscloud.AVUser
-import com.lapism.searchview.Search
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.content_main.*
 import layout.com.anew.easyItalian.WordList.WordsListsPage
 import layout.com.anew.easyItalian.read.ReadActivity
-import layout.com.anew.easyItalian.recite.DaoOpt
 import layout.com.anew.easyItalian.recite.ReciteWordAcitivity
-import layout.com.anew.easyItalian.recite.Word
-import org.json.JSONArray
-import java.io.ByteArrayOutputStream
 import java.io.File
-import java.io.InputStream
-import java.net.HttpURLConnection
-import java.net.URL
 
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -149,7 +139,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
         val headerView = navigationView.getHeaderView(0)
         val profile = headerView.findViewById<CircleImageView>(R.id.profile_picture)
-    profile.setOnClickListener(){
+        profile.setOnClickListener(){
        //     Toast.makeText(this,"call 个人资料 activity",Toast.LENGTH_SHORT).show()
             val changeToPersonalInfoActivity = Intent()
             changeToPersonalInfoActivity.setClass(this,PersonalInfo::class.java)
@@ -161,72 +151,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         searchWordButton.setOnClickListener(){
          //   AVUser.logOut()// 清除缓存用户对象
             // use handler to refresh ui
-            val mApp = application as BaseApplication
-            mApp.mHandler!!.sendEmptyMessage(1)
-             Toast.makeText(this,"call 查单词 activity",Toast.LENGTH_SHORT).show()
-        }
+            val intent = Intent(this,SearchView::class.java)
+            startActivity(intent)
 
-
-        searchBar.setOnQueryTextListener(object:Search.OnQueryTextListener{
-
-            override fun onQueryTextSubmit(query:CharSequence):Boolean{
-
-                    //searchBar.setMicIcon(R.drawable.ic_search_black)
-                    StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
-                            .detectDiskReads().detectDiskWrites().detectNetwork()
-                            .penaltyLog().build())
-                    StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
-                            .detectLeakedSqlLiteObjects().detectLeakedClosableObjects()
-                            .penaltyLog().penaltyDeath().build())
-                    val text=searchBar.getText().toString()
-                    //Toast.makeText(this,"search"+text,Toast.LENGTH_SHORT).show()
-                    //searchBar.setQuery(text,true)
-                    if(text.length!=0){
-
-                        val my = DaoOpt.getInstance()
-                        val mList=my.queryForWord(this@MainActivity,text)
-
-                        if(mList?.size!=0){
-                            val word : Word?=mList?.get(0)
-                            val data = arrayListOf(word?.word,word?.transform,word?.translation,word?.example)
-                            Toast.makeText(this@MainActivity,"search"+word?.word,Toast.LENGTH_SHORT).show()
-
-                            val changeToSearchWordPage = Intent()
-                            changeToSearchWordPage.setClass(this@MainActivity, SearchWordPage::class.java)
-                            changeToSearchWordPage.putStringArrayListExtra("data",data)
-                            startActivity(changeToSearchWordPage)
-                        }
-                        else{
-                            val word : Word?= Word()
-                            word?.setWord(text)
-                            //word?.setTranslation(getTranslation(text))
-                            //val data = arrayListOf(word?.word,word?.transform,word?.translation,word?.example)
-                            //      val translation=getTranslation(text)
-                            val translation=getDetails(text)
-                            Toast.makeText(this@MainActivity,translation,Toast.LENGTH_SHORT).show()
-                            val data = arrayListOf(word?.word,translation)
-                            val changeToSearchWordPage = Intent()
-                            changeToSearchWordPage.setClass(this@MainActivity, SearchWordPage::class.java)
-                            changeToSearchWordPage.putStringArrayListExtra("data",data)
-                            startActivity(changeToSearchWordPage)
-                        }
-                    }
-                //Toast.makeText(this@MainActivity,"1"+searchBar.getText().toString(),Toast.LENGTH_SHORT).show()
-                return false
-            }
-            override fun onQueryTextChange(newText: CharSequence):Unit{
-                //Toast.makeText(this@MainActivity,"2",Toast.LENGTH_SHORT).show()
-
-            }
-        })
-
-
-
-        searchBar.setOnMicClickListener(){
-
-            if(searchBar.isOpen) {
-                searchBar.close()
-            }
         }
 
 
@@ -295,84 +222,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
-
-    fun streamToString(myis: InputStream): String? {
-        try {
-            val out = ByteArrayOutputStream()
-            val buffer = ByteArray(1024)
-            var len = myis.read(buffer)
-            while (len != -1) {
-                out.write(buffer, 0, len)
-                len = myis.read(buffer)
-            }
-            out.close()
-            myis.close()
-            val byteArray = out.toByteArray()
-            return String(byteArray)
-        } catch (e: Exception) {
-            return null
-        }
-    }
-
-    fun getTranslation(word :String):String{
-        var result = ""
-        try{
-            val googleTranslate = "https://translate.google.cn/translate_a/single?client=gtx&sl=it&tl=zh-CN&dt=t&q="
-            val translateUrl = googleTranslate+word
-            val url = URL(translateUrl)
-            val urlConn = url.openConnection() as HttpURLConnection
-            // 设置连接主机超时时间
-            urlConn.connectTimeout = 5 * 1000
-            //设置从主机读取数据超时
-            urlConn.readTimeout = 5 * 1000
-            // 设置是否使用缓存  默认是true
-            urlConn.useCaches = false
-            // 设置为Post请求
-            urlConn.requestMethod = "GET"
-            //urlConn设置请求头信息
-            val USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
-            urlConn.setRequestProperty("User-Agent", USER_AGENT)
-            urlConn.connect()
-            val statusCode = urlConn.responseCode
-            var googleResult = ""
-            if (statusCode == 200) {
-                // 获取返回的数据
-                googleResult = streamToString(urlConn.getInputStream()) ?:""
-            }
-
-            val jsonArray = JSONArray(googleResult).getJSONArray(0)
-            for (i in 0 until jsonArray.length()) {
-                result += jsonArray.getJSONArray(i).getString(0)
-            }
-            Toast.makeText(this,result,Toast.LENGTH_SHORT).show()
-            return result
-        }catch (e :Exception){
-            e.printStackTrace();
-            result = "";
-        }
-        return result
-    }
-
-    fun getDetails(word :String):String{
-        val result:String
-        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
-                .detectDiskReads().detectDiskWrites().detectNetwork()
-                .penaltyLog().build())
-        StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
-                .detectLeakedSqlLiteObjects().detectLeakedClosableObjects()
-                .penaltyLog().penaltyDeath().build())
-        val query = AVQuery<AVObject>("Words")
-        if (query.whereEqualTo("word",word).first!=null){
-            result = query.whereEqualTo("word",word).first["details"].toString()
-        }else  {
-
-            result=getTranslation(word)
-        }
-
-
-        return result
-    }
-
 
 
 }
--- app/src/main/java/layout/com/anew/easyItalian/SearchWordPage.kt
+++ app/src/main/java/layout/com/anew/easyItalian/SearchWordPage.kt
@@ -1,11 +1,8 @@
 package layout.com.anew.easyItalian
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.media.AudioManager
 import android.media.MediaPlayer
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
@@ -13,7 +10,6 @@ import android.widget.Button
 import android.widget.ListView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_word_imformation.*
-import layout.com.anew.easyItalian.recite.ReciteWordAcitivity
 import layout.com.anew.easyItalian.recite.Word
 
 class SearchWordPage : Activity() {
@@ -51,7 +47,7 @@ class SearchWordPage : Activity() {
         }
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
-        val listView = findViewById<ListView>(R.id.wordDetails)
+        val listView = findViewById<View>(R.id.wordDetails) as ListView
         listView.adapter = adapter
 
         voice.setOnClickListener{
